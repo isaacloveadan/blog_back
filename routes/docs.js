12e0a74b6b952bf16b629790b5b598f456e567af
皓天 <273070111@qq.com>
2018-04-20 17:34:50 +0800

    [add]增加逻辑

--- routes/docs.js
+++ routes/docs.js
@@ -18,20 +18,62 @@ connection.connect();
 
 // 查询文章列表
 router.get('/list', function(req, res, next) {
-  connection.query('SELECT * FROM docs', function(err, result, fields) {
+  if (JSON.stringify(req.query) === '{}') {
+    connection.query('SELECT * FROM docs', function(err, result, fields) {
+      if (err) {
+        res.status(500);
+        res.send(err);
+      }
+      const data = formatResult(result);
+      _.forEach(data, (item, index) => {
+        const tmp = fs.readFileSync(item.contentUrl, 'utf-8');
+        data[index].content = tmp;
+        delete data[index].contentUrl;
+      })
+      res.status(200);
+      res.json({result: true, message: data})
+    })
+  } else {
+    const key = Object.keys(req.query)[0];
+    const value = req.query[key];
+    console.log(key);
+    console.log(value);
+    connection.query(`SELECT * FROM docs WHERE ${key} LIKE '%${value}%'`, function(err, result, fields) {
+      if (err) {
+        res.status(500);
+        res.send(err);
+      }
+      res.status(200);
+      res.json({result: true, message: formatResult(result)})
+    })
+    
+  }
+})
+
+// 新增文章
+router.post('/newList', function (req, res, next) { 
+  const randomTxt = Math.random().toString(36).substr(2);
+  const content = req.body.content;
+  const contentUrl = `C:/Users/isaac/Desktop/work/MyBlog/docs/${randomTxt}.txt`;
+  fs.writeFile(contentUrl, content, function(err) {
     if (err) {
       res.status(500);
       res.send(err);
     }
-    const data = formatResult(result);
-    _.forEach(data, (item, index) => {
-      const tmp = fs.readFileSync(item.contentUrl, 'utf-8');
-      console.log(tmp)
-      data[index].content = tmp;
-      delete data[index].contentUrl;
+    connection.query('INSERT INTO docs (title, description, contentUrl, user) VALUES (?, ?, ?, ?)', [
+      req.body.title,
+      req.body.description,
+      contentUrl,
+      req.body.user,
+    ], function(err, result, fields) {
+      if (err) {
+        res.status(500);
+        res.send(err);
+      }
+      res.status(200);
+      res.json({result: true, message: '发布成功'})
     })
-    res.status(200);
-    res.json({result: true, message: data})
   })
 })
+
 module.exports = router;
